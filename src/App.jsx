3f66707d5f0768ae3525150ed0ca8eb7cca695f8
S Hegde <shreyas567880@gmail.com>
2025-07-15 16:02:54 -0400

    Added an info section under Main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from './components/Header.jsx'
+import Main from './components/Main.jsx' 
+import Card from  './components/Card.jsx'
 
 function App() {
   return(
-    <>
+    <div className='bg-indigo-50 h-screen flex flex-col'>
       <Header />
-    </>
+      <Main />
+    </div>
   )
 } 
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+function Main(){
+    return(
+        <main className="flex flex-col mt-20">
+            <div className="max-w-4xl mx-auto px-4">
+                <h2 className="text-indigo-500 font-bold text-2xl border-b border-indigo-100 pb-2 mb-5">How It Works</h2> 
+                <p className="text-gray-500 text-sm leading-relaxed">Tailwind is designed for rapid development of modern applications. At its core, it is a robust mobile-first design system built with developer experience in mind. After using Tailwind CSS, you will find it difficult to go back to use anything else. Learn all about it in this all-inclusive course.</p> 
+                <h2 className="text-indigo-500 font-bold text-2xl border-b border-indigo-100 pb-2 mt-10 mb-5">Advantages</h2> 
+            </div> 
+        </main>
+    )
+} 
+
+export default Main;
