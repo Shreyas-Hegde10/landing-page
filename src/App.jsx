9a8ee8ad50e963d0c7685438d9ed62752aa921a4
S Hegde <shreyas567880@gmail.com>
2025-07-17 01:47:20 -0400

    Passed props to Card component using cardData.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx' 
-import Card from  './components/Card.jsx'
+import Card from  './components/Card.jsx' 
+import cardData from './cardData.js'
 
 function App() {
+  const cards = cardData.map((card) => {
+    return(
+      <Card
+      key={card.id}
+      {...card}/>
+    )
+  })
+
   return(
     <div className='bg-indigo-50 h-screen flex flex-col'>
       <Header />
-      <Main />
+      <Main /> 
+      {cards}
     </div>
   )
 } 
